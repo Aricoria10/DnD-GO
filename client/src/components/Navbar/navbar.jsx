ccc53f99539747aca015c4e71c16b8cb87f00690
jazminnova <jasminnova@gmail.com>
2024-03-07 10:50:28 -0500

    added in navbar skeleton

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,26 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
-const Header = () => {
+const Navbar = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+    <Navbar className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">D&D-GO</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
             <>
               <Link className="btn btn-lg btn-info m-2" to="/me">
-                {/* TODO: display the user's username */}'s profile
+                {/* TODO: display the user's username */} Create Character
+              </Link>
+              <Link className="btn btn-lg btn-info m-2" to="/me">
+                {/* TODO: display the user's username */} Choose Character
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/about">
+                About Us {/* Add "About Us" link */}
               </Link>
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
@@ -34,12 +39,15 @@ const Header = () => {
               <Link className="btn btn-lg btn-light m-2" to="/signup">
                 Signup
               </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/about">
+                About Us {/* Add "About Us" link */}
+              </Link>
             </>
           )}
         </div>
       </div>
-    </header>
+    </Navbar>
   );
 };
 
-export default Header;
+export default Navbar;
