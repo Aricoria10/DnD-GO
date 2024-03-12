95bb2dba889e477a33b3346a4667d987b97da306
jazminnova <jasminnova@gmail.com>
2024-03-12 18:44:00 -0400

    added aboutus comp

---
+++ client/src/components/About-us/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="py-8">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-semibold">About Us</h2>
+        <p className="text-lg">D&D-GO</p>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,6 @@
 // import { useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
 
-
-
 const Home = () => {
   // const { loading, data } = useQuery(QUERY_THOUGHTS);
   // const thoughts = data?.thoughts || [];
@@ -10,13 +8,10 @@ const Home = () => {
   return (
     <main>
       <Link className="btn btn-lg btn-info m-2" to="/encounter">
-                {/* TODO: display the user's username */} Start Encounter
-      </Link>      
+        {/* TODO: display the user's username */} Start Encounter
+      </Link>
     </main>
   );
 };
 
 export default Home;
-
-
-
