1fc597dbfa4868548104c7f34c6e5d3238826598
jazminnova <jasminnova@gmail.com>
2024-03-13 11:00:18 -0400

    changed footer

--- client/src/components/AboutUs/index.jsx
+++ client/src/components/AboutUs/index.jsx
@@ -6,7 +6,15 @@ function AboutUs() {
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-semibold">About Us</h2>
         <p className="text-lg">D&D-GO</p>
-        <p>dujdhjhcjdffhjfdjhvjffhjdhfjdhv</p>
+        <p>
+          {" "}
+          Dive into the world of D&D-GO, a unique fusion of Pokémon GO and
+          Dungeons & Dragons. Explore your surroundings to encounter mythical
+          monsters and engage in thrilling battles, all while leveling up your
+          character and earning experience points. With D&D-inspired elements
+          woven into every aspect, this game brings the magic of D&D to life in
+          the palm of your hand.{" "}
+        </p>
       </div>
     </div>
   );
