9b8843fb0879f86f1ed04315220c9acc2b93bd8a
a-nord <anord99@yahoo.com>
2024-03-12 22:36:45 -0400

    fixed the paths

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import AboutUs from "../components/AboutUs";
 import TeamMembers from "../components/TeamMembers";
 
-function Navbar() {
-  return <div>{/* Navbar */}</div>;
-}
-
 function App() {
   return (    
       <div>       
